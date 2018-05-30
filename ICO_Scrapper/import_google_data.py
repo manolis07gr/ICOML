bb2c92657276fd5750128d1c05eb8d7c405b88d6
manoschatzopoulos <chatzopoulos@phys.lsu.edu>
2018-05-30 09:21:38 -0500

    added column data for google news hits, final ico_data_full.csv now available for use

--- ICO_Scrapper/import_google_data.py
+++ ICO_Scrapper/import_google_data.py
@@ -6,18 +6,18 @@ with open("outdata/ico_data_full.csv") as f1:
     reader1 = csv.reader(f1)
     data1 = [r for r in reader1]
 
-with open("outdata/ico_google.csv") as f2:
+with open("outdata2/ico_data_full.csv") as f2:
     reader2 = csv.reader(f2)
     data2 = [r for r in reader2]
 
 with open('outdata/ico_google_new.csv', 'w') as csvfile_b:
     writer=csv.writer(csvfile_b, delimiter=',')
 
-    for i in range(0,np.shape(data1)[0]):
-        col2 = 0
+    for i in range(1,np.shape(data1)[0]):
+        col2 = 'N/A'
         for j in range(1,np.shape(data2)[0]):
             if data2[j][0] == data1[i][0]:
-                col2 = data2[j][1]
+                col2 = data2[j][13]
 
         writer.writerow([data1[i][0],col2])
 
