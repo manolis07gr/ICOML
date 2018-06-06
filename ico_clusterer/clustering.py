e1e75fb045b787f06639bac21881c4edaf7d0b33
MS <4848612+MSKX@users.noreply.github.com>
2018-06-06 09:18:08 +0100

    New data clusters + regression on clusters + more automation

--- ico_clusterer/clustering.py
+++ ico_clusterer/clustering.py
@@ -1,33 +1,31 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import shutil
 
 from sklearn.cluster import KMeans
-from core.utilities import export_cluster_radar_json, construct_cluster_data
+from core.utilities import export_cluster_radar_json, construct_cluster_data, export_cluster_regression
 
-raw_ico_data = pd.read_csv('./data/ICO_data_refactored.csv')
+raw_ico_data = pd.read_csv('../data/ico_data_raw.csv')
 
-ico_model_features = ['age_days', 'country', 'industry',
-                      'hardcap', 'money_raised', 'pct_complete',
-                      'hardcap_reached', 'token_prx', 'followers',
-                      'team_size', 'cum_years_experience', 'accredited_advisors',
-                      'media_mentions', 'listing_prx', 'log_rets']
+ico_model_features = ['age', 'region', 'industry',
+                      'hardcap', 'raised',
+                      'telegram', 'team', 'N_google_news', 'N_twitter',
+                      'price', 'ret_ico_to_day_one']
 
-radar_chart_features = ['money_raised',
-                        'team_size',
-                        'accredited_advisors',
-                        'cum_years_experience',
-                        'age_days']
+radar_chart_features = ['age', 'hardcap', 'raised',
+                        'telegram', 'team', 'N_google_news', 'N_twitter',
+                        'price', 'ret_ico_to_day_one']
 
 raw_ico_data = raw_ico_data.dropna()
 
 ico_data_model = raw_ico_data[ico_model_features]
-ico_data_model = pd.get_dummies(data=ico_data_model, columns=['country', 'industry', 'hardcap_reached'])
+ico_data_model = pd.get_dummies(data=ico_data_model, columns=['region', 'industry'])
 
 clustering_clusters = 7
 
 k_means_model = KMeans(n_clusters=clustering_clusters, random_state=0).fit(ico_data_model)
 
-log_returns = ico_data_model['log_rets'].as_matrix()
+log_returns = ico_data_model['ret_ico_to_day_one'].as_matrix()
 
 #plt.scatter(k_means_model.labels_, log_returns, c=k_means_model.labels_)
 #plt.title("Cluster of log returns")
@@ -35,7 +33,10 @@ log_returns = ico_data_model['log_rets'].as_matrix()
 
 raw_ico_data['cluster'] = k_means_model.labels_
 
-print("""{\"name\": \"ICO Ecosystem\",\"children\": [""")
+
+ico_ecosystem_file = open("./output/ico_clusters.json", "w+")
+
+print("""{\"name\": \"ICO Ecosystem\",\"children\": [""", file=ico_ecosystem_file)
 money_raised_max = 0
 money_raised_min = 10000000000
 
@@ -45,38 +46,43 @@ for radar_chart_feature in radar_chart_features:
 
 no_icos = 0
 labels = set(k_means_model.labels_)
-labels = set([6])
 no_clusters = len(labels)
 consolidated_cluster_data = construct_cluster_data(raw_ico_data, labels)
 
+
 for c in labels:
-    print("""{\"name\": \"Cluster """, c + 1, """\",\"children\": [""")
+    print("""{\"name\": \"Cluster """, c + 1, """\",\"children\": [""", file=ico_ecosystem_file)
     ix = 0
     cluster_data = consolidated_cluster_data[consolidated_cluster_data['cluster'] == c]
     for row in cluster_data.iterrows():
         no_icos += 1
-        current_money_raised = row[1]['money_raised']
+        current_money_raised = row[1]['raised']
         if current_money_raised > money_raised_max:
-            strongest_ico = row[1]['ico_name']
+            strongest_ico = row[1]['coin']
             money_raised_max = current_money_raised
         if current_money_raised < money_raised_min:
-            weakest_ico = row[1]['ico_name']
+            weakest_ico = row[1]['coin']
             money_raised_min = current_money_raised
 
         for radar_chart_feature in radar_chart_features:
             avg_cluster_data[radar_chart_feature] += int(row[1][radar_chart_feature])
 
-        print("""{\"name\": \"""", row[1]['ico_name'], """\",\"loc\":""", row[1]['money_raised'], """}""", end='')
-        if ix + 1 < len(cluster_data.index):
-            print(",")
-        ix += 1
-    print("""]}""", end='')
-    if c < no_clusters-1:
-        print(",")
-    print("""]}""")
+        print("""{\"name\": \"""", row[1]['coin'], """\",\"loc\":""", row[1]['raised'], end='', file=ico_ecosystem_file)
 
-strongest_ico_data = raw_ico_data[raw_ico_data['ico_name'] == strongest_ico]
-weakest_ico_data = raw_ico_data[raw_ico_data['ico_name'] == weakest_ico]
+        if ix + 1 < len(cluster_data.index):
+            print("},",  file=ico_ecosystem_file)
+        else:
+            print("}", file=ico_ecosystem_file)
+        ix += 1
+
+    if c < no_clusters-1:
+        print("]},", file=ico_ecosystem_file)
+    else:
+        print("]}", file=ico_ecosystem_file)
+print("""]}""", file=ico_ecosystem_file)
+
+strongest_ico_data = raw_ico_data[raw_ico_data['coin'] == strongest_ico]
+weakest_ico_data = raw_ico_data[raw_ico_data['coin'] == weakest_ico]
 
 for radar_chart_feature in radar_chart_features:
     avg_cluster_data[radar_chart_feature] /= no_icos
@@ -88,4 +94,39 @@ print("-----------------------------")
 print("Winning ICO:", strongest_ico_data)
 print("Weakest ICO:", weakest_ico_data)
 
-export_cluster_radar_json(radar_chart_features, strongest_ico_data, weakest_ico_data, avg_cluster_data)
+file_name = "radar_ecosystem_data.json"
+export_cluster_radar_json(radar_chart_features, strongest_ico_data, weakest_ico_data, avg_cluster_data, file_name)
+
+export_cluster_regression(consolidated_cluster_data, "ecosystem_regression.json")
+
+for c in labels:
+    cluster_data = consolidated_cluster_data[consolidated_cluster_data['cluster'] == c]
+    for row in cluster_data.iterrows():
+        no_icos += 1
+        current_money_raised = row[1]['raised']
+        if current_money_raised > money_raised_max:
+            strongest_ico = row[1]['coin']
+            money_raised_max = current_money_raised
+        if current_money_raised < money_raised_min:
+            weakest_ico = row[1]['coin']
+            money_raised_min = current_money_raised
+
+        for radar_chart_feature in radar_chart_features:
+            avg_cluster_data[radar_chart_feature] += int(row[1][radar_chart_feature])
+
+    strongest_ico_data = raw_ico_data[raw_ico_data['coin'] == strongest_ico]
+    weakest_ico_data = raw_ico_data[raw_ico_data['coin'] == weakest_ico]
+
+    for radar_chart_feature in radar_chart_features:
+        avg_cluster_data[radar_chart_feature] /= no_icos
+        avg_cluster_data[radar_chart_feature] = round(avg_cluster_data[radar_chart_feature], 2)
+
+    print("ICO CLUSTER", str(c))
+    print("-----------------------------")
+    print("Winning ICO:", strongest_ico_data)
+    print("Weakest ICO:", weakest_ico_data)
+
+    file_name = "radar_cluster_"+str(c+1)+".json"
+    export_cluster_radar_json(radar_chart_features, strongest_ico_data, weakest_ico_data, avg_cluster_data, file_name)
+    regression_file_name = "cluster_" + str(c + 1) + "_regression.json"
+    export_cluster_regression(consolidated_cluster_data, regression_file_name)
--- ico_clusterer/core/utilities.py
+++ ico_clusterer/core/utilities.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from math import log10
-
+import statsmodels.formula.api as sm
 
 def construct_cluster_data(raw_ico_data, clusters):
     consolidated_cluster_data = pd.DataFrame()
@@ -13,32 +13,42 @@ def construct_cluster_data(raw_ico_data, clusters):
 def export_cluster_radar_json(f_radar_chart_features,
                               f_strongest_ico_data,
                               f_weakest_ico_data,
-                              f_avg_dict):
+                              f_avg_dict, out_file_name):
 
+    path= "./output/" + out_file_name
+    file_name = open(path, "w+")
     no_features = len(f_radar_chart_features)
     i = 0
-    print("""[""", file=open("output.txt", "w"))
+    print("""[""", file=file_name)
     for radar_chart_feature in f_radar_chart_features:
-        print("""{\"feature\":\"""", radar_chart_feature, """\",""", file=open("output.txt", "a"))
+        print("""{\"feature\":\"""", radar_chart_feature, """\",""", file=file_name)
         v = f_strongest_ico_data[radar_chart_feature].iloc(0)[0]
         if v >=1:
-            print("""\"Strongest\":""", round(log10(v), 2), """,""", file=open("output.txt", "a"))
+            print("""\"Strongest\":""", round(log10(v), 2), """,""", file=file_name)
         else:
-            print("""\"Strongest\": 0,""", file=open("output.txt", "a"))
+            print("""\"Strongest\": 0,""", file=file_name)
 
         v = f_weakest_ico_data[radar_chart_feature].iloc(0)[0]
         if v >=1:
-            print("""\"Weakest\":""", round(log10(v), 2), """,""", file=open("output.txt", "a"))
+            print("""\"Weakest\":""", round(log10(v), 2), """,""", file=file_name)
         else:
-            print("""\"Weakest\": 0,""", file=open("output.txt", "a"))
+            print("""\"Weakest\": 0,""", file=file_name)
         v = f_avg_dict[radar_chart_feature]
         if v >= 1:
-            print("""\"Average\":""", round(log10(v), 2), file=open("output.txt", "a"))
+            print("""\"Average\":""", round(log10(v), 2), file=file_name)
         else:
-            print("""\"Average\":0""", file=open("output.txt", "a"))
+            print("""\"Average\":0""", file=file_name)
         if i < no_features - 1:
-            print("},", file=open("output.txt", "a"))
+            print("},", file=file_name)
         else:
-            print("}", file=open("output.txt", "a"))
+            print("}", file=file_name)
         i += 1
-    print("""]""", file=open("output.txt", "a"))
+    print("""]""", file=file_name)
+
+
+def export_cluster_regression(frame, out_file_name):
+    path = "./output/" + out_file_name
+    file_name = open(path, "w+")
+
+    result = sm.ols(formula="raised ~ age + team", data=frame).fit()
+    print(result.summary(), file=file_name)
